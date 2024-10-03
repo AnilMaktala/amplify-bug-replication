ae526a57569e471fd04cca8fdc92ad72145b1418
Anil Maktala <amaktala@amazon.com>
2024-10-03 11:05:26 -0400

    updated schema

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,9 +1,9 @@
-import { defineBackend } from '@aws-amplify/backend';
+import { defineBackend } from "@aws-amplify/backend";
 import { StartingPosition } from "aws-cdk-lib/aws-lambda";
 import { DynamoEventSource } from "aws-cdk-lib/aws-lambda-event-sources";
-import { auth } from './auth/resource';
-import { data } from './data/resource';
-import { myDynamoDBFunction } from './functions/dynamoDB-function/resource';
+import { auth } from "./auth/resource";
+import { data } from "./data/resource";
+import { myDynamoDBFunction } from "./functions/dynamoDB-function/resource";
 
 const backend = defineBackend({
   auth,
@@ -11,8 +11,8 @@ const backend = defineBackend({
   myDynamoDBFunction,
 });
 
-const eventSource = new DynamoEventSource(backend.data.resources.tables["Todo"], {
-  startingPosition: StartingPosition.LATEST,
-});
+// const eventSource = new DynamoEventSource(backend.data.resources.tables["Todo"], {
+//   startingPosition: StartingPosition.LATEST,
+// });
 
-backend.myDynamoDBFunction.resources.lambda.addEventSource(eventSource);
+//backend.myDynamoDBFunction.resources.lambda.addEventSource(eventSource);
--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -1,6 +1,11 @@
-import { type ClientSchema, a, defineData, defineFunction } from "@aws-amplify/backend";
+import {
+  type ClientSchema,
+  a,
+  defineData,
+  defineFunction,
+} from "@aws-amplify/backend";
 
-const echoHandler = defineFunction({ entry: './echo-handler/handler.ts' });
+const echoHandler = defineFunction({ entry: "./echo-handler/handler.ts" });
 
 const schema = a.schema({
   EchoResponse: a.customType({
@@ -10,21 +15,35 @@ const schema = a.schema({
 
   echo: a
     .query()
-    .arguments({ content: a.string() })
+    .arguments({ content: a.string(), status: a.boolean() })
     .returns(a.ref("EchoResponse"))
-    .authorization(allow => [allow.authenticated()])
+    .authorization((allow) => [allow.authenticated()])
     .handler(a.handler.function(echoHandler)),
-
+  Measures: a.customType({
+    ts: a.integer(),
+    on: a.boolean(),
+    standby: a.boolean(),
+    tankPct: a.float(),
+    tankRem: a.float(),
+    bmsPct: a.float(),
+    bmsRem: a.float(),
+    flow: a.float(),
+    outPsi: a.float(),
+    outKpa: a.float(),
+  }),
   Todo: a
-    .model({ content: a.string(), })
+    .model({
+      content: a.string(),
+      status: a.boolean(),
+    })
     .authorization((allow) => [allow.guest()]),
-})
+});
 
 export type Schema = ClientSchema<typeof schema>;
 
 export const data = defineData({
   schema,
   authorizationModes: {
-    defaultAuthorizationMode: 'iam',
+    defaultAuthorizationMode: "iam",
   },
 });
